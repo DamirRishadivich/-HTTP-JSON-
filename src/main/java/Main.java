14c1116b237ea5f740bdb24e46aec77abb2c95fc
Damir <klivecar@yandex.ru>
2022-12-07 20:50:54 +0300

    InitialCommit

---
+++ src/main/java/Main.java
@@ -0,0 +1,49 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class Main {
+    public static final String URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(URL);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+        readAnswer(body);
+
+        httpClient.close();
+    }
+
+    static void readAnswer(String body) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Post> posts = mapper.readValue(body, new TypeReference<>() {});
+        posts.stream().filter(x -> x.getUpvotes() != 0).
+                forEach(System.out::println);
+    }
+}
